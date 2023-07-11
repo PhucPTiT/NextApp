cebd63374de3cfcb30d468ea7a82c1bc7a2399ff
PhucPTiT <phucndah2002@gmail.com>
2023-07-11 17:28:57 +0700

    push feature/AuthLogin UI

--- MyApp/messenger-clone/app/(site)/compomnents/AuthForm.tsx
+++ MyApp/messenger-clone/app/(site)/compomnents/AuthForm.tsx
@@ -4,6 +4,8 @@ import Button from '../../components/Button';
 import Input from '../../components/inputs/Input';
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import AuthSocialButton from './AuthSocialButton';
+import {BsGithub, BsGoogle} from 'react-icons/bs'
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -64,9 +66,52 @@ const AuthForm = () => {
                     <Input id="email" label="Email" register={register} errors={errors} required />
                     <Input id="password" label="Password" register={register} errors={errors} required />
                     <div>
-                        <Button>{variant === 'LOGIN' ? 'Sign in' : 'Register'}</Button>
+                        <Button disabled= {isLoading} fullWidth type = "submit">{variant === 'LOGIN' ? 'Sign in' : 'Register'}</Button>
                     </div>
                 </form>
+                <div className='mt-6'>
+                    <div className='relative'>
+                        <div className='
+                            absolute
+                            inset-0
+                            flex
+                            items-center
+                        '>
+                            <div className='w-full border-gray-300 border-t'/>
+                        </div>
+                        <div className='relative flex justify-center text-sm'>
+                            <span className='bg-white px-2 text-gray-500'>
+                                Or continue width
+                            </span>
+                        </div>
+                    </div>
+                    <div className='mt-6 flex gap-2'>
+                        <AuthSocialButton 
+                            icon = {BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+                        <AuthSocialButton 
+                            icon = {BsGoogle}
+                            onClick={() => socialAction('google')}
+                        />
+                    </div>
+                    <div className='
+                        flex
+                        justify-center
+                        gap-2
+                        text-sm
+                        mt-6
+                        px-2
+                        text-gray-500
+                    '>
+                        <div>
+                            {variant === 'LOGIN' ? 'New to Messenger ?' : 'Already account'}
+                        </div>
+                        <div onClick={toggleVariant} className='underline cursor-pointer'>
+                            {variant === "LOGIN" ? "Creat an account" : "Login"}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
